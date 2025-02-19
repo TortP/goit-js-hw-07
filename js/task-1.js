d856ecc47e80c9499198392c0053c2019ebc27b2
TortP <104321524+TortP@users.noreply.github.com>
2025-02-19 21:49:07 +0100

    solutin added

--- js/task-1.js
+++ js/task-1.js
@@ -1,9 +1,12 @@
-function makeTransaction(quantity, pricePerDroid) {
-  return `You ordered ${quantity} droids worth ${
-    quantity * pricePerDroid
-  } credits!`;
-}
+const categoriesList = document.querySelector('#categories');
+const categoriesItems = categoriesList.querySelectorAll('li.item');
 
-console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+console.log(`Total categories: ${categoriesItems.length}`);
+
+categoriesItems.forEach((item) => {
+  const categoryTitle = item.querySelector('h2').textContent;
+  const categoryItemsCount = item.querySelectorAll('li').length;
+
+  console.log(`Category: ${categoryTitle}`);
+  console.log(`Items count: ${categoryItemsCount}`);
+});
--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,7 @@
-function getElementWidth(content, padding, border) {
-  return (
-    Number.parseFloat(content) +
-    Number.parseFloat(padding) * 2 +
-    Number.parseFloat(border) * 2
-  );
-}
+const nameInput = document.querySelector('#name-input');
+const nameOutput = document.querySelector('#name-output');
 
-console.log(getElementWidth('50px', '8px', '4px')); // 74
-console.log(getElementWidth('60px', '12px', '8.5px')); // 101
-console.log(getElementWidth('200px', '0px', '0px')); // 200
+nameInput.addEventListener('input', () => {
+  const trimmedValue = nameInput.value.trim();
+  nameOutput.textContent = trimmedValue || 'Anonymous';
+});
